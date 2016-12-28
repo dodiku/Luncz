a175cfe47514ea14afb83ae4d8406490e17ccaa6
Dror Ayalon <drorayalon@Drors-MacBook-Pro.local>
2016-12-28 16:15:21 -0500

    .

--- input.py
+++ input.py
@@ -5,28 +5,59 @@ import datetime
 import librosa
 import numpy as np
 import csv
+import os
+import shutil
+from time import sleep
+
+# MOVING ALL EXISTING FILES TO A BACKUP DIRECTORY
+
+source = './_recordings/'
+destination = './_recordings/backup/'
+
+
+# old_files = os.listdir(source)
+# print (old_files)
+# for file in old_files:
+# 	if not file == ".DS_Store":
+# 		file_path = source + file
+# 		shutil.move(file_path, destination) 
 
 # RECORDING A SOUND USING PYAUDIO
 CHUNK = 1024
 FORMAT = pyaudio.paInt16
 CHANNELS = 1
 RATE = 44100
-RECORD_SECONDS = 1
+RECORD_SECONDS = 6
 AUDIO_OUTPUT_TYPE = ".wav"
 WAVE_OUTPUT_FILENAME_NO_EXTENSION = "_recordings/" + datetime.datetime.now().isoformat()
 WAVE_OUTPUT_FILENAME = "_recordings/" + datetime.datetime.now().isoformat() + AUDIO_OUTPUT_TYPE
 
 audio = pyaudio.PyAudio()
 
+print ('* * * * * * * * * * * * * * * * * * * * * *')
+print ('1...')
+print ('* * * * * * * * * * * * * * * * * * * * * *')
+sleep(1)
+
+print ('* * * * * * * * * * * * * * * * * * * * * *')	
+print ('2...')
+print ('* * * * * * * * * * * * * * * * * * * * * *')
+sleep(1)
+
+print ('* * * * * * * * * * * * * * * * * * * * * *')
+print ('3...')
+print ('* * * * * * * * * * * * * * * * * * * * * *')
+sleep(1)
+
+print ('* * * * * * * * * * * * * * * * * * * * * *')
+print ('RECORDING...')
+print ('* * * * * * * * * * * * * * * * * * * * * *')
+
 stream = audio.open(format=FORMAT,
                 	channels=CHANNELS,
                 	rate=RATE,
                 	input=True,
                 	frames_per_buffer=CHUNK)
-
-print ('* * * * * * * * * * * * * * * * * * * * * *')		
-print ('RECORDING...')
-print ('* * * * * * * * * * * * * * * * * * * * * *')    
 
 frames = []
 
@@ -68,6 +99,13 @@ beat_times = librosa.frames_to_time(beat_frames, sr=sr)
 librosa.output.times_csv(CSV_FILENAME, beat_times)
 print ('beat_times.csv done')
 
+# WRITING A FILE WITH THE TEMPO
+TEXT_FILENAME = WAVE_OUTPUT_FILENAME_NO_EXTENSION + ".txt"
+bpm_value = open(TEXT_FILENAME, 'w')
+tempo_text = str(tempo) + '\n'
+bpm_value.write(tempo_text)
+
+
 """""""""""""""""""""""""""""""""""""""
 3 - get notes
 """""""""""""""""""""""""""""""""""""""
@@ -84,43 +122,75 @@ print (len(hz[0]))
 # print (notes)
 
 ## GET STRONGEST OCTAVE
-# strongest_octave = 0
-# strongest_octave_sum = 0
-# for octave in range(len(hz)):
-# 	sum = 0
-# 	for frame in hz[octave]:
-# 		sum = sum + frame
-# 	if sum > strongest_octave_sum:
-# 		strongest_octave_sum = sum
-# 		strongest_octave = octave
+strongest_octave = 0
+strongest_octave_sum = 0
+for octave in range(len(hz)):
+	sum = 0
+	for frame in hz[octave]:
+		sum = sum + frame
+	if sum > strongest_octave_sum:
+		strongest_octave_sum = sum
+		strongest_octave = octave
 
-# print ('strongest octave is:')
-# print (strongest_octave)
-# print ('strength is:')
-# print (strongest_octave_sum)
+print ('strongest octave is:')
+print (strongest_octave)
+print ('strength is:')
+print (strongest_octave_sum)
+
 
 ## GET HEIGHEST HZ FOR EACH TIME FRAME
 strongest_hz = []
-
 for i in range(len(hz[0])):
 	strongest_hz.append(0)
+
+notes = []
+for i in range(len(hz[0])):
+	notes.append(0)
 
 print ('begining')
 print (strongest_hz)
 
-for frame_i in range(len(hz[0])): # number of frames
-	print ('frame begins')
-	for octave_i in range(len(hz)): # number of notes * number of octaves
+for frame_i in range(len(hz[0])):
+	# print ('frame begins')
+	strongest_temp = 0
+	for octave_i in range(len(hz)):
 
 		# print (hz[octave_i][frame_i])
-		print (octave_i, hz[octave_i], strongest_hz[frame_i])
-		# print (octave_i, hz[octave_i], strongest_hz[frame_i])
-		# print (strongest_hz[frame_i])
+		# print (octave_i, hz[octave_i][frame_i], strongest_temp, strongest_hz[frame_i])
 		# if hz[octave_i][frame_i] != 1:
+		# print (hz[octave_i][frame_i])
+		# print (strongest_hz[frame_i])
+		if hz[octave_i][frame_i] > strongest_temp:
+			strongest_temp = hz[octave_i][frame_i]
+			strongest_hz[frame_i] = octave_i + 1
+			notes[frame_i] = librosa.hz_to_note(hz[octave_i][frame_i])
 
-		if hz[octave_i][frame_i] > strongest_hz[frame_i]:
-			strongest_hz[frame_i] = octave_i
+print ('the notes are:')
+print (strongest_hz)
 
+# C C# D D# E F F# G G# A  A# B
+# 1 2  3 4  5 6 7  8 9  10 11 12
+strongest_hz_sum = [0,0,0,0,0,0,0,0,0,0,0,0]
+for note in strongest_hz:
+	# print (note)
+	strongest_hz_sum[note-1] = strongest_hz_sum[note-1] + 1
+
+print ('the strong notes (sum) are:')
+print (strongest_hz_sum)
+
+for i in range(len(strongest_hz_sum)):
+	# print (strongest_hz_sum[i], len(strongest_hz), (strongest_hz_sum[i] / len(strongest_hz)))
+	strongest_hz_sum[i] = float(strongest_hz_sum[i]) / len(strongest_hz)
+	
+
+print ('the percentages are:')
+print (strongest_hz_sum)
+
+notes_sorted = [0,0,0,0,0,0,0,0,0,0,0,0]
+for num in range(len(notes_sorted)):
+	 biggest = strongest_hz_sum.index(max(strongest_hz_sum))
+	 notes_sorted[num] = biggest+1
+	 strongest_hz_sum[biggest] = strongest_hz_sum[biggest] - 0.25
 
 # for i in range(len(hz[0])):
 # 	for octave_i in range(len(hz)):
@@ -128,9 +198,16 @@ for frame_i in range(len(hz[0])): # number of frames
 # 			strongest_hz[i] = octave[i]
 
 # strongest_hz.append(??)
-print ('the strong notes are:')
-print (strongest_hz)
-print (len(strongest_hz))
+
+
+print ('the strong notes sorted:')
+print (notes_sorted)
+
+for note in notes_sorted:
+	note_string = str(note) + '\n'
+	bpm_value.write(note_string)
+
+bpm_value.close()
 
 """""""""""""""""""""""""""""""""""""""
 3 - get timbre
--- output.py
+++ output.py
@@ -3,7 +3,6 @@ import librosa
 import numpy as np
 import csv
 import ctcsound
-# import glob
 import os
 import shutil
 import csv
@@ -16,6 +15,7 @@ version = 0
 
 # C C# D D# E F F# G G# A  A# B
 # 1 2  3 4  5 6 7  8 9  10 11 12
+
 cpspch_array = [7.00, 7.01, 7.02, 7.03, 7.04, 7.05, 7.06, 7.07, 7.08, 7.09, 7.10, 7.11, 7.12]
 
 source = './_recordings/'
@@ -40,7 +40,6 @@ if ret == ctcsound.CSOUND_SUCCESS:
 	cs.start()
 	pt = ctcsound.CsoundPerformanceThread(cs.csound())
 	pt.play()
-	# pt.scoreEvent(False, 'i', (102, 1, 1, 0.2, 0))
 	while not cs.performBuffer():
 
 		# SEARCHING FOR NEW CSV FILES ON THE '_RECORDINGS' DIRECTORY
@@ -69,16 +68,12 @@ if ret == ctcsound.CSOUND_SUCCESS:
 					else:
 						data.append(int(line[0]))
 
-					# print ('tempo line:', line)
-					# print (data)
-					# data.append(9)
 				recording_tempo = data[0]
 				print ('tempo float (recording_tempo):', recording_tempo)
     			print ('data:', data)
 		for file in new_files:
 			if file.endswith(".csv"):
 
-				# pt.scoreEvent(False, 'i', (100, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3))
 				version = version + 1
 				pt.scoreEvent(False, 'i', (101, 0, 0.0001, version))
 
@@ -95,71 +90,15 @@ if ret == ctcsound.CSOUND_SUCCESS:
 				print ('beat list:', beat_list)
 
 				for beat in beat_list:
-					# original_beat_times.append(float(beat)*60/recording_tempo)
 					original_beat_times.append(float(beat))
 
 
 				print ('original beat onset times:', original_beat_times)
 
-				# CALCULATE NOTE DURATION
-				# note_duration = 10*60/recording_tempo
-				# print ('note durations:', note_duration)
-
-    			# CALCULATE NOTES ONSET TIMES
-    			# last_beat_ends = 0
-    			# next_beat_starts = 0
-    			# for beat in range(len(original_beat_times)):
-    			# 	print ('aaa')
-    			# 	if beat == 0:
-    			# 		last_beat_ends = original_beat_times[beat] + note_duration
-    			# 		tempo_beat_times.append(original_beat_times[beat])
-    			# 		continue
-    			# 	else:
-    			# 		print ('bbb')
-    			# 		# last_beat_ends = original_beat_times[beat-1] 
-    			# 		next_beat_starts = original_beat_times[beat]
-    			# 		previous_beat_starts = original_beat_times[beat-1]
-    			# 		delta = next_beat_starts - previous_beat_starts
-    			# 		run = True
-    			# 		while run == True:
-    			# 			if (next_beat_starts - tempo_beat_times[beat-1]) < note_duration:
-    			# 				next_beat_starts = next_beat_starts + delta
-    			# 			else:
-    			# 				next_beat_starts = next_beat_starts - delta
-    			# 				tempo_beat_times.append(next_beat_starts)
-    			# 				run = False
-    			# 				print('while ended')
-
-    					# while note_duration >= (next_beat_starts - last_beat_ends):
-    					# 	print (next_beat_starts, original_beat_times[beat], original_beat_times[beat-1], last_beat_ends)
-    					# 	next_beat_starts = next_beat_starts + (original_beat_times[beat] - original_beat_times[beat-1])
-    					# print('while ended')
-    					# next_beat_starts = next_beat_starts - (original_beat_times[beat] - original_beat_times[beat-1])
-    					# tempo_beat_times.append(next_beat_starts)
-    					# last_beat_ends = next_beat_starts
-					# print ('new beat onset times:', tempo_beat_times)				
-
-				# ADDING SCORE EVENTS
-				# delay_time = recording_tempo/60*5
-				
-				# last_beat = original_beat_times[len(original_beat_times)-1]
-				# first_beat = original_beat_times[0]
-				# next_first_beat = 5 - last_beat + first_beat
-				# repeat = 1
-				# number_of_notes = 0
 				n = 1
 				print ('====== sending =======')
-				# pt.scoreEvent(False, 'i', (100, 0, 1, 0.2, cpspch_array[data[n]], version, 1, recording_tempo))
-				# print (100, 0, 1, 0.2, cpspch_array[data[n]], version, 1, recording_tempo)
+
 				for time in original_beat_times:
-					# pt.scoreEvent(False, 'i', (100, time, 1, 0.2, time+5, number_of_notes, next_first_beat))
-
-					# pt.scoreEvent(False, 'i', (p1, p2, p3, p4, p5, p6))
-					# 90*2/60
-
-					# s_per_beat = 60 / recording_tempo
-					# s_per_measure = s_per_beat * number_of_beats
-					# loop_length = s_per_measure * loop_measures
 
 					s_per_beat = 60 / recording_tempo
 					s_per_measure = s_per_beat * len(beat_list)
@@ -173,8 +112,6 @@ if ret == ctcsound.CSOUND_SUCCESS:
 					if (loop_length - modified_time) < 0:
 						continue
 
-					# pt.scoreEvent(False, 'i', (100, modified_time, 1, 0.2, cpspch_array[data[n]], version, 1, recording_tempo, loop_length))
-					# print (100, modified_time, 1, 0.2, cpspch_array[data[n]], version, 1, recording_tempo, loop_length)
 					if 6 - modified_time < 0.8:
 						pt.scoreEvent(False, 'i', (100, modified_time, 1, 0, cpspch_array[data[n]], version, 1, recording_tempo, loop_length))
 					else:
@@ -182,83 +119,6 @@ if ret == ctcsound.CSOUND_SUCCESS:
 						print (100, modified_time, 1, 0.2, cpspch_array[data[n]], version, 1, recording_tempo, loop_length)
 					n = n+1
 				print ('====== end =======')
-
-
-
-				# note_number = 0
-				# time = 0
-				# freq = 0
-				# delta = 0
-				# try:
-				# 	original_beat_times[0]
-				# 	time1 = original_beat_times[0]
-				# 	freq1 = cpspch_array[data[1]-1]
-				# 	# freq1 = 220
-				# 	# print (data[1]-1, cpspch_array[data[1]-1])
-				# 	note_number = 1
-				# 	delta = 5 - original_beat_times[0]
-				# except:
-				# 	time1 = 0
-				# 	freq1 = 0
-
-				# try:
-				# 	original_beat_times[1]
-				# 	time2 = original_beat_times[1]
-				# 	# freq2 = 220
-				# 	freq2 = cpspch_array[data[2]-1]
-				# 	# print (data[2]-1, cpspch_array[data[2]-1])
-				# 	note_number = 2
-				# 	delta = 5 - original_beat_times[1]
-				# except:
-				# 	time2 = 0
-				# 	freq2 = 0
-
-				# try:
-				# 	original_beat_times[2]
-				# 	time3 = original_beat_times[2]
-				# 	freq3 = cpspch_array[data[3]-1]
-				# 	# freq3 = 220
-				# 	# print (data[3]-1, cpspch_array[data[3]-1])
-				# 	note_number = 3
-				# 	delta = 5 - original_beat_times[2]
-				# except:
-				# 	time3 = 0
-				# 	freq3 = 0
-
-				# try:
-				# 	original_beat_times[3]
-				# 	time4 = original_beat_times[3]
-				# 	freq4 = cpspch_array[data[4]-1]
-				# 	# freq4 = 220
-				# 	# print (data[4]-1, cpspch_array[data[4]-1])
-				# 	note_number = 4
-				# 	delta = 5 - original_beat_times[3]
-				# except:
-				# 	time4 = 0
-				# 	freq4 = 0
-
-					
-				# version = version + 1
-				# # print ('====== clearing score (that will take 5 seconds) =======')
-				# # sleep(0.5)
-				# if recording_tempo == 0:
-				# 	recording_tempo = 60
-				# print ('====== sending =======')
-				# print (100, 0, 1, time, freq, time1, freq1, time2, freq2, time3, freq3, time4, freq4, delta, note_number, 1, version, recording_tempo)
-				# print ('====== end =======')
-				# pt.scoreEvent(False, 'i', (100, 0, 1, time, freq, time1, freq1, time2, freq2, time3, freq3, time4, freq4, delta, note_number, 1, version, recording_tempo))
-
-					# number_of_notes = number_of_notes + 1
-					# print ('number_of_notes:', number_of_notes)
-					# print ('next first beat:', next_first_beat)
-    				# beat_time = previous_beat_time + note_duration + beat
-    				# previous_beat_time = beat_time
-    				# beat_in_tempo = beat 
-    				
-    				# previous_beat_time = beat_time
-				# pt.scoreEvent(False, 'i', (100, (original_beat_times[len(original_beat_times)-1]+1), 5, 0))
-				# pt.scoreEvent(False, 'i', (100, (tempo_beat_times[len(tempo_beat_times)-1]+note_duration), 5, 0.20))
-		
 
 				# MOVING ALL EXISTING FILES TO A BACKUP DIRECTORY
 				old_files = os.listdir(source)
@@ -270,17 +130,6 @@ if ret == ctcsound.CSOUND_SUCCESS:
 
 				print ("-=-=-=-=-=-END-=-=-=-=-=-")
 
-				
-
-		
-
-		# CLEARING THE DIRECTORY
-
-		# 
-
-		### look for a new file....
-
-
 		cs.sleep(5000)
 
 	pt.stop()
@@ -288,22 +137,3 @@ if ret == ctcsound.CSOUND_SUCCESS:
 
 
 del cs
-
-# ret = cs.compileCsdText(csd)
-# if ret == ctcsound.CSOUND_SUCCESS:
-# 	cs.start()
-# 	# cs.perform()
-# 	# cs.reset()
-
-# 	pt = ctcsound.CsoundPerformanceThread(cs.csound())
-# 	pt.play()
-
-# 	pt.scoreEvent(False, 'i', (1, 0, 2, 0.5, 8.06, 0.05, 0.3, 0.5))
-
-# 	cs.sleep(2000)
-
-# 	pt.stop()
-# 	pt.join()
-
-
-# del cs
